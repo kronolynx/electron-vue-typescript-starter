00f249bda4a659ab0031e57d2f16ab1da7df752a
Johann Og <kronolynx@gmail.com>
2018-04-19 17:09:28 +0200

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,23 @@
+
+<template lang="pug">
+  #app Basic pug {{ msg }}
+    #nav 
+      router-link(to="/") Home
+      |  |  
+      router-link(to="/counter") Counter 
+    router-view
+</template>
+
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      msg: "boilerplate"
+    }
+  }
+})
+</script>
+
---
+++ src/renderer/components/sections/Home.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div
+    //- img#logo(src="~@src/renderer/assets/logo.png")
+    h3.title Welcome 
+    | This is the Home page {{`@src`}}
+</template>
+
+<script lang='ts'>
+import Vue from "vue"
+import Component from "vue-class-component"
+
+@Component
+export default class Home extends Vue {}
+</script>
+
+
+
+
